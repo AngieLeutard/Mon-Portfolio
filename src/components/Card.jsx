38811b58b642277cad5c1ce008b2030af49a245f
angieleutard <leutardangie@MacBook-Pro-de-leutard.local>
2023-06-22 11:31:16 +0200

    add cards changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,12 @@
 import "../index.scss"
 
-import image from "../assets/cards_pictures/booki.png"
 import PropTypes from 'prop-types';
 
 function Card({picture, title}) {
     return (
         <div className="card_wrapper">
-            <img src={ image } alt="cardPicture" className="card_picture" />
-            {/* <span className='card_title'>{ title }</span> */}
+            <img src={ picture } alt="cardPicture" className="card_picture" />
+            <p className="card_text">Langages & Outils</p>
         </div>
     )
 }
--- src/containers/Projets.jsx
+++ src/containers/Projets.jsx
@@ -1,29 +1,48 @@
 import "../index.scss"
 
-import Projets from "../projets.json"
 import Card from "../components/Card"
+import booki from "../assets/cards_pictures/booki.png"
+import ohmyfood from "../assets/cards_pictures/ohmyfood.png"
+import printit from "../assets/cards_pictures/printit.png"
+import sophie from "../assets/cards_pictures/sophie.png"
+import qwenta from "../assets/cards_pictures/qwenta.png"
+import kasa from "../assets/cards_pictures/kasa.png"
+import nina from "../assets/cards_pictures/nina.png"
+import event from "../assets/cards_pictures/77.png"
+import bank from "../assets/cards_pictures/argent.png" 
 
 function SectionProjets() {
     return (
         <div className='projets_wrapper' id="about">
             <h2 className="projets_title">Mes réalisations</h2>
             <section className="projetsCards_wrapper">
-            {/* {Projets.map((projet) => (
-            <Card
-                key={`${projet.name}`}
-                picture={projet.picture}
-                title={projet.name}
-            />
-        ))} */}
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
-                <Card/>
+                <Card
+                    picture={ booki }
+                />
+                <Card
+                    picture={ ohmyfood }
+                />
+                <Card
+                    picture={ printit }
+                />
+                <Card
+                    picture={ sophie }
+                />
+                <Card
+                    picture={ qwenta }
+                />
+                <Card
+                    picture={ kasa }
+                />
+                <Card
+                    picture={ nina }
+                />
+                <Card
+                    picture={ event }
+                />
+                <Card
+                    picture={ bank }
+                />
             </section>
         </div>
     )
